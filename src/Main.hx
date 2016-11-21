9b6e95803f85bfabd43897eb727dc8b32153ff6d
kevinresol <kevinresol@gmail.com>
2016-11-21 11:47:46 +0800

    In a usable state

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import haxe.Json;
 import Sys.*;
 import sys.io.Process;
 
@@ -12,48 +13,67 @@ class Main {
 	static function main()
 		new Main();
 		
+	var cwd:String;
+		
 	public function new() {
 		var libPath = getCwd();
 		var args = args();
-		var cwd = args.pop();
+		cwd = args.pop();
+		setCwd(cwd);
+		
 		var hxml = switch args {
 			case []:
-				findHxml(cwd);
+				findHxml();
 			case [v] if(v.extension() == 'hxml'):
 				if(!'$cwd$v'.exists()) fail('The specified hxml does not exist ($cwd$v)') else v;
 			default:
 				showUsage();
 		}
 		
-		prepareFolders(cwd);
+		prepare();
 		
-		var libs = parseHxml('$cwd$hxml');
-		for(lib in libs) {
+		var libs = parseHxml(hxml);
+		
+		function handleLib(lib) {
+			if(submoduleExists('haxelib/$lib')) {
+				info('Submodule $lib already exists, skipping');
+				return;
+			}
 			var url = getLibUrl(lib);
-			trace(url);
+			addSubmodule(lib, url);
+			var haxelibJson = findHaxelibJson('haxelib/$lib/');
+			var libs = parseHaxelibJson(haxelibJson);
+			info(libs.length > 0 ? '$lib has the following dependencies: ' + libs.join(', ') : '$lib has no dependencies');
+			for(lib in libs) handleLib(lib);
 		}
 		
+		for(lib in libs) handleLib(lib);
+		
+		info('Done!');
 	}
 	
-	function prepareFolders(path:String) {
-		function create(folder:String) {
-			var f = path + folder;
+	function prepare() {
+		var git = '.git';
+		if(!git.exists() || !git.isDirectory()) fail('Current directory is not a git repo ($cwd)');
+		function create(f:String) {
 			if(!f.exists()) f.createDirectory();
 		}
 		create('.haxelib');  
 		create('haxelib');  
 	}
 	
-	function findHxml(path:String) {
-		for(f in path.readDirectory()) {
-			if(!'$path$f'.isDirectory() && f.extension() == 'hxml')
+	function findHxml() {
+		info('Finding hxml');
+		for(f in cwd.readDirectory()) {
+			if(!f.isDirectory() && f.extension() == 'hxml')
 				return f;
 		}
-		throw fail('Cannot find hxml file in current directory ($path)');
+		throw fail('Cannot find hxml file in current directory ($cwd)');
 	}
 	
-	function parseHxml(path:String) {
-		var content = path.getContent();
+	function parseHxml(file:String) {
+		info('Parsing hxml');
+		var content = file.getContent();
 		var libs = [];
 		var tokens = content.replace('\n', ' ').replace('\t', ' ').split(' ');
 		var i = 0;
@@ -64,11 +84,70 @@ class Main {
 		return libs;
 	}
 	
+	function findHaxelibJson(path:String) {
+		info('Finding haxelib.json in $path');
+		
+		function find(dir:String) {
+			for(f in dir.readDirectory()) {
+				var full = dir.addTrailingSlash() + f;
+				if(full.isDirectory()) switch find(full) {
+					case null: // continue
+					case v: return v;
+				}
+				if(f == 'haxelib.json') return full;
+				if(f == 'haxelib.xml') {
+					info('Converting haxelib.xml ($full)');
+					setCwd(dir);
+					var proc = new Process('haxelib', ['convertxml']);
+					proc.exitCode();
+					setCwd(cwd);
+					return full.withoutExtension().withExtension('json');
+				}
+			}
+			return null;
+		} 
+		return find(path);
+	}
+	
+	function parseHaxelibJson(path:String) {
+		info('Parsing haxelib.json at ${path.directory()}');
+		var json = Json.parse(path.getContent());
+		return json.dependencies == null ? [] : Reflect.fields(json.dependencies);
+	}
+	
 	function getLibUrl(name:String) {
+		info('Getting the url for $name');
 		var proc = new Process('haxelib', ['info', name]);
 		var out = proc.stdout.readAll().toString();
-		for(line in out.split('\n')) if(line.startsWith('Website:')) return line.replace('Website:', '').trim();
-		return null;
+		for(line in out.split('\n')) if(line.startsWith('Website:')) {
+			var url = line.replace('Website:', '').trim();
+			if(url != '') return url;
+			break;
+		}
+		return prompt('Cannot find the url of $name from lib.haxe.org, please input mannually:');
+	}
+	
+	function addSubmodule(name:String, url:String) {
+		var dest = 'haxelib/$name';
+		info('Adding $name as submodule from $url');
+		var proc = new Process('git', ['submodule', 'add', url, dest]);
+		var out = proc.stdout.readAll().toString();
+		var err = proc.stderr.readAll().toString();
+		if(proc.exitCode() != 0) fail(err);
+	}
+	
+	var submoduleRe = ~/\[submodule "([^"]*)"\]/;
+	function submoduleExists(relPath:String) {
+		var f = '.gitmodules';
+		if(!f.exists()) return false;
+		for(line in f.getContent().split('\n')) if(submoduleRe.match(line) && submoduleRe.matched(1) == relPath) return true;
+		return false;
+	}
+	
+	function prompt(msg:String) {
+		println('Prompt: $msg');
+		print('> ');
+		return stdin().readLine();
 	}
 	
 	function showUsage<T>():T {
@@ -77,9 +156,13 @@ class Main {
 		return null;
 	}
 	
+	function info(msg:String, ?pos:haxe.PosInfos) {
+		println('Info: $msg');
+	}
+	
 	function fail<T>(msg:String, ?pos:haxe.PosInfos):T {
 		println('Error: $msg');
-		exit(0);
+		exit(1);
 		return null;
 	}
 }
